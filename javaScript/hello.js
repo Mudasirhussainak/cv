c0c5aa1b4e314758759ad1f1836f588200fcf635
Mudasirhussainak <109842126+Mudasirhussainak@users.noreply.github.com>
2023-01-21 02:43:56 +0530

    Delete javaScript directory

--- javaScript/hello.js
+++
@@ -1,7 +0,0 @@
-const prompt= require('prompt-sync')();
-var name=prompt("WHAT IS YOUR NAME :");
-var hername=prompt("WHAT IS HIS/HER NAME :");
-var love= Math.random() *100;
-love=Math.floor(love)+1;
-
-console.log (name  +" " + "and"+ " "+ hername +" "+"your love score is"+ " "+ love +"%");
